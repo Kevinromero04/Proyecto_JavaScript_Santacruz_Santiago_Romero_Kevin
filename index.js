c54614a2bc847e26c3bd4bd0d6ee6d5e7553fedc
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-08-24 16:18:40 -0500

    feat: :sparkles: Fusion de modulos y apartado de personajes casi terminado
    fusione prueba con index hice que el codigo funcionara cuando le dieramos click al boton de personajes
    
    @By Kevinromero04

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const lista = document.querySelector("ul");
-const filtrados2 = ["alirio", "juan x2", "soata", "mariana", "camila"];
+const url_people = "https://www.swapi.tech/api/people/";
+const url_especies = "https://www.swapi.tech/api/species/";
+const url_vehiculos = "https://www.swapi.tech/api/vehicles/";
+const url_starchips = "https://www.swapi.tech/api/starships/";
+const url_planets = "https://www.swapi.tech/api/planets/";
+var jiji = 0;
 
-var cosa = [];
-for (const resultado of filtrados2) {
-  cosa.push("<li>" + resultado + " eliminado ❌ " + "</li>");
-  console.log(resultado);
+var nn = document.querySelector("#jaja");
+nn.addEventListener("click", async (e) => {
+  jiji += 1;
+  console.log(jiji);
+
+  await buscar_uno_personaje22(jiji);
+});
+async function buscar_uno_personaje22(jiji) {
+  if (jiji >= 1) {
+    const lista2 = document.querySelector(".contenedor");
+
+    try {
+      let vacio = [];
+      for (let numeral = 1; numeral < 83; numeral++) {
+        if (numeral === 17) {
+          numeral += 1;
+        }
+        let res = await fetch(url_people + numeral);
+        let respuesta = await res.json();
+        const devol = document.createElement("ul");
+        devol.id = "uno_por_uno";
+        console.log("El personaje: " + respuesta.result.properties.name);
+        vacio.push("El personaje: " + respuesta.result.properties.name);
+        vacio.push("Tiene las siguientes características:");
+        vacio.push("Color de pelo: " + respuesta.result.properties.hair_color);
+        vacio.push("Color de piel: " + respuesta.result.properties.skin_color);
+        vacio.push("Color de ojos: " + respuesta.result.properties.eye_color);
+        vacio.push("Género: " + respuesta.result.properties.gender);
+        vacio.push("Estatura: " + respuesta.result.properties.height);
+        vacio.push("Masa: " + respuesta.result.properties.mass);
+        vacio.push("id " + numeral);
+        for (const element of vacio) {
+          console.log(element);
+          let anuel = document.createElement("li");
+          anuel.textContent = element;
+          devol.appendChild(anuel);
+        }
+        vacio.length = 0;
+        lista2.appendChild(devol);
+      }
+    } catch (error) {
+      console.error(error, "Algo salió mal");
+    }
+  }
 }
-for (const element of cosa) {
-  lista.innerHTML += element;
-}
+
+buscar_uno_personaje22();
--- prueba.js
+++ prueba.js
@@ -1,69 +1,68 @@
 function los_filtros() {
-    const lista = document.querySelector("ul");
-    const filtrados2 = ["alirio", "juan x2", "soata", "mariana", "camila"];
-    
-    var cosa = [];
-    for (const resultado of filtrados2) {
-      cosa.push("<li>" + resultado + " eliminado ❌ " + "</li>");
-      console.log(resultado);
-    }
-    for (const element of cosa) {
-      lista.innerHTML += element;
-    }
-    }
-    
-    los_filtros()
-    
-    
-    
-    const url_people = "https://www.swapi.tech/api/people/";
-    const url_especies = "https://www.swapi.tech/api/species/";
-    const url_vehiculos = "https://www.swapi.tech/api/vehicles/";
-    const url_starchips = "https://www.swapi.tech/api/starships/";
-    const url_planets = "https://www.swapi.tech/api/planets/";
-    
-    
-    async function buscar_uno_personaje22(num) {
-        const lista2 = document.querySelector(".contenedor");
-        const devol = document.createElement("ul")
-        devol.id = "uno_por_uno"
-          if (num > 80) {
-            console.log("Solo hay 80 personajes intenta de nuevo");
-          } else if (num === 0) {
-            console.log("saliendo");
-            return ;
-          } else if (num === 17) {
-            console.log("Este personaje ya no existe");
-          } else {
-            try {
-              let res = await fetch(url_people + num);
-              let respuesta = await res.json();
-              let vacio = []
-                vacio.push("El personaje: " + respuesta.result.properties.name);
-                vacio.push("Tiene las siguientes características:");
-                vacio.push("Color de pelo: " + respuesta.result.properties.hair_color);
-                vacio.push("Color de piel: " + respuesta.result.properties.skin_color);
-                vacio.push("Color de ojos: " + respuesta.result.properties.eye_color);
-                vacio.push("Género: " + respuesta.result.properties.gender);
-                vacio.push("Estatura: " + respuesta.result.properties.height);
-                vacio.push("Masa: " + respuesta.result.properties.mass);
-                
-              for (const element of vacio) {
-                let anuel = "<li>" + element + "</li>"
-                //var hola = devol.replaceChild += element
-                // lista2.innerHTML += hola;
-                console.log(anuel)
-               
-              }
-              
-              return
-            } catch (error) {
-              console.error(error, "Algo salió mal");
-            }
-          }
+  const lista = document.querySelector("ul");
+  const filtrados2 = ["alirio", "juan x2", "soata", "mariana", "camila"];
 
+  var cosa = [];
+  for (const resultado of filtrados2) {
+    cosa.push("<li>" + resultado + " eliminado ❌ " + "</li>");
+    console.log(resultado);
+  }
+  for (const element of cosa) {
+    lista.innerHTML += element;
+  }
+}
+var jiji = 1;
+var nn = document.querySelector("#jaja");
+nn.addEventListener("click", (e) => {
+  num += 1;
+});
+//los_filtros();
+
+const url_people = "https://www.swapi.tech/api/people/";
+const url_especies = "https://www.swapi.tech/api/species/";
+const url_vehiculos = "https://www.swapi.tech/api/vehicles/";
+const url_starchips = "https://www.swapi.tech/api/starships/";
+const url_planets = "https://www.swapi.tech/api/planets/";
+
+async function buscar_uno_personaje22(jiji) {
+  if (jiji > 0) {
+    const lista2 = document.querySelector(".contenedor");
+
+    try {
+      let vacio = [];
+      for (let numeral = 1; numeral < 7; numeral++) {
+        if (numeral === 17) {
+          numeral += 1;
+        }
+        let res = await fetch(url_people + numeral);
+        let respuesta = await res.json();
+        const devol = document.createElement("ul");
+        devol.id = "uno_por_uno";
+        console.log("El personaje: " + respuesta.result.properties.name);
+        vacio.push("El personaje: " + respuesta.result.properties.name);
+        vacio.push("Tiene las siguientes características:");
+        vacio.push("Color de pelo: " + respuesta.result.properties.hair_color);
+        vacio.push("Color de piel: " + respuesta.result.properties.skin_color);
+        vacio.push("Color de ojos: " + respuesta.result.properties.eye_color);
+        vacio.push("Género: " + respuesta.result.properties.gender);
+        vacio.push("Estatura: " + respuesta.result.properties.height);
+        vacio.push("Masa: " + respuesta.result.properties.mass);
+
+        vacio.push("id " + numeral);
+        vacio.push("*******************************************************");
+        for (const element of vacio) {
+          console.log(element);
+          let anuel = document.createElement("li");
+          anuel.textContent = element;
+          devol.appendChild(anuel);
+        }
+        vacio.length = 0;
+        lista2.appendChild(devol);
       }
-      buscar_uno_personaje22(num)
-    
-    
-    
+    } catch (error) {
+      console.error(error, "Algo salió mal");
+    }
+  }
+}
+
+buscar_uno_personaje22();
