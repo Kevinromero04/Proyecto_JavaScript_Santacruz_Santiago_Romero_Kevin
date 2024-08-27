2684075db43fdf0b9583e03b1bb8736f19f1fcae
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-08-27 12:43:52 -0500

    feat: :bug: codigo refactorizado
    By @Kevinromero

--- index.js
+++ index.js
@@ -3,60 +3,49 @@ const url_especies = "https://www.swapi.tech/api/species/";
 const url_vehiculos = "https://www.swapi.tech/api/vehicles/";
 const url_starchips = "https://www.swapi.tech/api/starships/";
 const url_planets = "https://www.swapi.tech/api/planets/";
-var jiji = 0;
 
 var nn = document.querySelector("#jaja");
 nn.addEventListener("click", buscar_uno_personaje22)
 
+
 async function buscar_uno_personaje22() {
-    try {
+  try {
       var lista2 = document.querySelector(".contenedor");
       lista2.innerHTML = ''; 
       let vacio = [];
       for (let numeral = 1; numeral < 83; numeral++) {
-        if (numeral === 17) {
-          numeral += 1;
-        }
-        let res = await fetch(url_people + numeral);
-        let respuesta = await res.json();
-        const devol = document.createElement("ul");
-        devol.id = "uno_por_uno";
-        console.log("El personaje: " + respuesta.result.properties.name);
-        vacio.push("El personaje: " + respuesta.result.properties.name);
-        vacio.push("Tiene las siguientes características:");
-        vacio.push("Color de pelo: " + respuesta.result.properties.hair_color);
-        vacio.push("Color de piel: " + respuesta.result.properties.skin_color);
-        vacio.push("Color de ojos: " + respuesta.result.properties.eye_color);
-        vacio.push("Género: " + respuesta.result.properties.gender);
-        vacio.push("Estatura: " + respuesta.result.properties.height);
-        vacio.push("Masa: " + respuesta.result.properties.mass);
-        vacio.push("id " + numeral);
-        for (const element of vacio) {
-          console.log(element);
-          let anuel = document.createElement("li");
-          anuel.textContent = element;
-          devol.appendChild(anuel);
-        }
-        vacio.length = 0;
-        lista2.appendChild(devol);
-
+          if (numeral === 17) {
+              numeral += 1;
+          }
+          let res = await fetch(url_people + numeral);
+          let respuesta = await res.json();
+          const devol = document.createElement("ul");
+          devol.id = "uno_por_uno";
+          vacio.push("El personaje: " + respuesta.result.properties.name,"Tiene las siguientes características:","Color de pelo: " + respuesta.result.properties.hair_color,"Color de piel: " + respuesta.result.properties.skin_color,"Color de ojos: " + respuesta.result.properties.eye_color,"Género: " + respuesta.result.properties.gender,"Estatura: " + respuesta.result.properties.height,"Masa: " + respuesta.result.properties.mass,"id " + numeral)
+          for (const element of vacio) {
+              let anuel = document.createElement("li");
+              anuel.textContent = element;
+              devol.appendChild(anuel);
+          }
+          vacio.length = 0;
+          lista2.appendChild(devol);
       }
-    } catch (error) {
+  } catch (error) {
       console.error(error, "Algo salió mal");
-    }
   }
+}
 
-var contador = 0;
+
+
+
+
+
+
+
 var espe = document.querySelector("#esp");
-espe.addEventListener("click", async (e) => {
-  contador += 1;
-  console.log(contador);
+espe.addEventListener("click", todos_especies)
 
-  await todos_especies(contador);
-});
-async function todos_especies(contador) {
-  if (contador >= 1) {
-    
+async function todos_especies() {
     let lista2 = document.querySelector(".contenedor");
     lista2.innerHTML = ''; 
     console.log("Esto puede tomara de 1 min a 3 min");
@@ -69,36 +58,12 @@ async function todos_especies(contador) {
         const respuesta = await res.json();
         const ul_Espe = document.createElement("ul");
         ul_Espe.id = "uno_por_uno";
-        vacio.push(
-          "El nombre de la especie " + respuesta.result.properties.name
-        );
-        vacio.push("Tiene las siguentes caracteristicas: ");
-        vacio.push(
-          "Colores de pelo: " + respuesta.result.properties.hair_colors
-        );
-        vacio.push(
-          "Colores de cuerpo: " + respuesta.result.properties.skin_colors
-        );
-        vacio.push(
-          "Colores de ojos: " +
-          respuesta.result.properties.eye_colors,
-        );
-        vacio.push(
-          "Lenguaje: " +
-          respuesta.result.properties.language,
-        );
-        vacio.push(
-          "Altura promedio: " +
-          respuesta.result.properties.average_height,
-        );
-        vacio.push(
-          "Esperanza de vida: " +
-          respuesta.result.properties.average_lifespan
-        );
-        vacio.push(
-          "id: " +
-          num
-        );
+        vacio.push("El nombre de la especie " + respuesta.result.properties.name,"Tiene las siguentes caracteristicas: ","Colores de pelo: " + respuesta.result.properties.hair_colors,"Colores de cuerpo: " + respuesta.result.properties.skin_colors,"Colores de ojos: " +
+        respuesta.result.properties.eye_colors,"Lenguaje: " +
+        respuesta.result.properties.language,"Altura promedio: " +
+        respuesta.result.properties.average_height,"Esperanza de vida: " +
+        respuesta.result.properties.average_lifespan,"id: " +
+        num,)
         for (let element of vacio) {
           console.log(element);
           let anuel = document.createElement("li");
@@ -110,10 +75,7 @@ async function todos_especies(contador) {
       } catch (error) {
         console.error(error, "Algo salió mal");
       }
-    }
-
   }
   }
-todos_especies();
 
 //hacer que se vuelvan mas asincronas las funciones para que se pueda imprimir en orden y solo una vez 
